37afd9ca284e68a76aeab4bc78d953cf3d0280c4
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-07-21 00:14:49 +0300

    adding documentation and removing old versions

--- headPoseEstimation_last.py
+++ headPoseEstimation_last.py
@@ -32,6 +32,11 @@ def distance(p1, p2):
 
 
 def calculate_ear(eye_landmarks):
+    """
+    Calculate the Eye Aspect Ratio (EAR) of the eye.
+    :param eye_landmarks: List of eye landmarks.
+    :return: Eye Aspect Ratio (EAR) of the eye.
+    """
     if len(eye_landmarks) < 8:
         return None
 
@@ -51,9 +56,15 @@ def calculate_ear(eye_landmarks):
 
 right_eye_indices = [33, 160, 159, 158, 133, 153, 145, 144]
 left_eye_indices = [263, 387, 386, 385, 362, 380, 374, 373]
+inner_mouth_indices = [62, 12, 292, 15]
 
 
 def calculate_yawn_aspect_ratio(inner_mouth_landmarks):
+    """
+    Calculate the Yawn Aspect Ratio (YAR) of the mouth.
+    :param inner_mouth_landmarks: List of inner mouth landmarks.
+    :return: Yawn Aspect Ratio (YAR) of the mouth.
+    """
     if len(inner_mouth_landmarks) < 4:
         return None
 
@@ -69,7 +80,6 @@ def calculate_yawn_aspect_ratio(inner_mouth_landmarks):
     return yar
 
 
-inner_mouth_indices = [62, 12, 292, 15]
 
 # Open the video capture
 cap = cv2.VideoCapture(0)
@@ -90,12 +100,11 @@ while True:
     # Get the results from the face mesh model using the RGB image as input and store it in the results variable
     results = face_mesh.process(image_rgb)
 
-    # results.multi_face_landmarks contains the landmarks of all the faces detected in the image
-    # Each face_landmarks object contains the landmarks of a single face
-    # Each landmark object contains the x, y, and z coordinates of the landmark
-    # The x and y coordinates are normalized to the image width and height
-    # The z coordinate is the depth of the landmark from the camera
-    # The landmarks are stored in the face_landmarks.landmark object
+    """
+    The results variable contains the following attributes:
+    - multi_face_landmarks: The 3D landmarks of the face.
+    - multi_face_landmarks_2d: The 2D landmarks of the face.
+    """
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
             for idx, lm in enumerate(face_landmarks.landmark):
@@ -108,11 +117,6 @@ while True:
                 199: Chin
                 '''
                 if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
-                    '''
-                    if idx == 1:
-                        nose_2d = (lm.x * img_w, lm.y * img_h)
-                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
-                    '''
                     x, y = int(lm.x * img_w), int(lm.y * img_h)
 
                     # Get the 2D Coordinates
@@ -121,56 +125,26 @@ while True:
                     # Get the 3D Coordinates
                     face_3d.append([x, y, lm.z])
 
-    # If face_3d and face_2d are not empty, then we have detected a face
-    # We can use the 3D and 2D coordinates to estimate the head pose
-    # We can also use the 2D coordinates to draw the landmarks on the image
-    # We can also use the 3D coordinates to draw the bounding box around the face
-    # We can also use the 3D coordinates to calculate the head pose angles
-    # We can also use the 3D coordinates to calculate the eye aspect ratio
-    # We can also use the 2D coordinates to calculate the yawn aspect ratio
-    # We can also use the 2D coordinates to calculate if the driver is looking forward
-    # We can also use the 2D coordinates to calculate if the driver is sleeping
-    # We can also use the 2D coordinates to calculate if the driver is yawning
-    # We can also use the 2D coordinates to calculate if the driver is drowsy
-
         face_2d = np.array(face_2d, dtype=np.float64)
         face_3d = np.array(face_3d, dtype=np.float64)
-        # Camera Matrix
-        # The camera matrix is a 3x3 matrix that contains the intrinsic parameters of the camera
-        # The intrinsic parameters include the focal length, the optical center, and the skew coefficient
-        # The camera matrix is used to project the 3D points of the face to the 2D image plane
-        # The camera matrix is used to solve the Perspective-n-Point problem
-        # The camera matrix is used to find the rotation and translation vectors that map the 3D points to their 2D projections
-
+        """
+        Camera Matrix
+        The camera matrix is used to map the 3D points of the face to their 2D projections in image plane.
+        The camera matrix is used to find the rotation and translation vectors that map the 3D points of the face to
+        their 2D projections.
+        """
         focal_length = 1 * img_w
         cam_matrix = np.array([[focal_length, 0, img_h / 2],
                                [0, focal_length, img_w / 2],
                                [0, 0, 1]])
-        # Distortion Coefficients
-        # The distortion coefficients are used to correct the radial and tangential lens distortions
-        # The distortion coefficients are used to undistort the image
-        # The distortion coefficients are used to correct the image for lens distortion
-        # The distortion coefficients are used to correct the image for radial and tangential distortions
-        # The distortion coefficients are used to correct the image for barrel and pincushion distortions
-        # The distortion coefficients are used to correct the image for wide-angle distortions
-        # The distortion coefficients are used to correct the image for fisheye distortions
-        # The distortion coefficients are used to correct the image for chromatic aberrations
-        # The distortion coefficients are used to correct the image for color fringing
-        # The distortion coefficients are used to correct the image for color bleeding
-        # The distortion coefficients are used to correct the image for color distortion
-        # The distortion coefficients are used to correct the image for color aberrations
-
+        """
+        Distortion Coefficients
+        The distortion coefficients are used to correct the radial and tangential lens distortion.
+        The distortion coefficients are used to find the rotation and translation vectors that map the 3D points of the
+        face to their 2D projections.
+        """
         dist_matrix = np.zeros((4, 1), dtype=np.float64)
-        '''
-        cv2.solvePnP: Solves the Perspective-n-Point problem to find the rotation and translation vectors that map
-        3D points (face_3d) to their corresponding 2D projections (face_2d) given the camera matrix and distortion
-        coefficients.
-        success: Boolean indicating if the function was successful.
-        rot_vec: Rotation vector (Rodrigues vector) representing the rotation of the face.
-        trans_vec: Translation vector representing the translation of the face.
-        '''
-        # Solve the Perspective-n-Point problem to find the rotation and translation vectors
-        # that map the 3D points of the face to their 2D projections
+
         '''
         cv2.solvePnP: Solves the Perspective-n-Point problem to find the rotation and translation vectors that map
         3D points (face_3d) to their corresponding 2D projections (face_2d) given the camera matrix and distortion
@@ -183,13 +157,8 @@ while True:
 
         # Convert Rotation Vector to Rotation Matrix
         # The rotation matrix represents the rotation of the face in the 3D space
-        # The rotation matrix is a 3x3 matrix that contains the rotation of the face
         rmat, jac = cv2.Rodrigues(rot_vec)
 
-        # Convert Rotation Matrix to Euler Angles
-        # The Euler angles represent the rotation of the face in the 3D space
-        # The Euler angles are represented in radians
-        # The Euler angles are represented in the order Z, Y, X (Yaw, Pitch, Roll)
         '''
         cv2.RQDecomp3x3: Decomposes the rotation matrix into the Euler angles.
         angles: Euler angles representing the rotation of the face.
@@ -213,18 +182,6 @@ while True:
         else:
             text = "Looking Forward"
             not_looking_forward_start_time = None
-
-        '''
-        - Project the 3D points of the nose to the 2D image plane
-        nose_3d_projection: The 2D coordinates of the projected nose tip on the image plane.
-        jacobian: The Jacobian matrix of partial derivatives of the 2D projected points with respect to the parameters.
-        '''
-        # nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
-
-        # p1 = (int(nose_2d[0]), int(nose_2d[1]))
-        # p2 = (int(nose_2d[0] + y * 10), int(nose_2d[1] - x * 10))
-
-        # cv2.line(frame, p1, p2, (255, 0, 0), 3)
 
         cv2.putText(frame, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
